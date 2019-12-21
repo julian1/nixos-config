d81a62a9c811cdd7cb3d42eb33e52b9cb0e60dcc
julian1 <git@julian1.io>
2019-12-21 10:28:51 +0700

    tidy

--- examples/network-transport-tcp.nix
+++ examples/network-transport-tcp.nix
@@ -3,8 +3,10 @@
 # https://github.com/haskell-distributed/network-transport-tcp/
 # https://stackoverflow.com/questions/27968909/how-to-get-cabal-and-nix-work-together
 
+# overriding...
 # https://github.com/Gabriel439/haskell-nix/tree/master/project4
 # https://stackoverflow.com/questions/56414329/how-do-i-override-a-haskell-package-with-a-git-local-package-with-nix-when-usi
+# https://old.reddit.com/r/NixOS/comments/ebgkub/need_help_trying_to_use_ghc_810_with_nix/
 
 # cd network-transport-tcp/
 # cabal2nix network-transport-tcp.cabal 
@@ -25,6 +27,7 @@ let
     };
   };
 
+  # nixpkgs = import ./nixpkgs.nix;
   pkgs = import <nixpkgs> { inherit config; };
 in
 with pkgs;
