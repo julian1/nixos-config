c7aaebe9f5209044d18eef2d45b8834aa3860ebf
julian1 <git@julian1.io>
2019-12-21 15:48:32 +0700

    update

--- examples/project-m36.nix
+++ examples/project-m36.nix
@@ -9,7 +9,7 @@
 # https://old.reddit.com/r/NixOS/comments/ebgkub/need_help_trying_to_use_ghc_810_with_nix/
 
 # to fix cd network-transport-tcp/
-# follow this, to create a local nix package, 
+# follow this, to create a local nix package, and make adjustments to cabal
 # https://nixos.org/nixpkgs/manual/#how-to-create-nix-builds-for-your-own-private-haskell-packages
 
 
@@ -24,29 +24,29 @@ let
       haskellPackages = pkgs.haskellPackages.override {
         overrides = haskellPackagesNew: haskellPackagesOld: rec {
 
-          # network-transport-tcp has dependency on old version of containers  
+          # network-transport-tcp-0.6.0 has a dependency on old version of containers
+          # so we need master branch of network-transport-tcp
           # also remove tests which are in a separate module from cabal file
           network-transport-tcp = (haskellPackagesNew.callPackage /home/me/nixos-config/nix/network-transport-tcp/shell.nix { });
 
           # test fail...  disable tests
+          # rank1dynamic-0.4.0
           rank1dynamic = pkgs.haskell.lib.dontCheck  haskellPackagesOld.rank1dynamic;
 
-          # distributed-process   has the same issue... with dependency on containers ==0.5.*, 
+          # distributed-process-0.7.4 also has the same issue... a dependency on containers ==0.5.*,
 
         };
       };
     };
   };
 
-  pkgs = import <nixpkgs> { inherit config; }; 
+  pkgs = import <nixpkgs> { inherit config; };
 in
 
 pkgs.stdenv.mkDerivation {
   name = "my-example";
 
   buildInputs = [
-    pkgs.cabal-install
-    pkgs.ghc
     # pkgs.haskellPackages.network-transport-tcp
 
     pkgs.haskellPackages.project-m36
