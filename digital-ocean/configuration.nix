fe1f3bc43847781fd48583fbbd2c31886c92ea4a
julian1 <git@julian1.io>
2019-12-23 12:53:39 +0000

    update

--- digital-ocean/configuration.nix
+++ digital-ocean/configuration.nix
@@ -5,9 +5,10 @@
   imports = lib.optional (builtins.pathExists ./do-userdata.nix) ./do-userdata.nix ++ [
     (modulesPath + "/virtualisation/digital-ocean-config.nix")
   ] 
-        ++ [ /home/me/nixos-config/digital-ocean/keys.nix ] 
-        ++ [ /home/me/nixos-config/digital-ocean/dotfiles.nix ] 
-  ;
+    ++ [ 
+    /home/me/nixos-config/digital-ocean/keys.nix  
+    /home/me/nixos-config/digital-ocean/dotfiles.nix 
+  ];
 }
 
 
