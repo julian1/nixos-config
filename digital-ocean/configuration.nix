e97c478a22884bb530312dd9b7c067d9b45dbddc
julian1 <git@julian1.io>
2019-12-23 12:51:06 +0000

    update

--- digital-ocean/configuration.nix
+++ digital-ocean/configuration.nix
@@ -1,10 +1,13 @@
-# /etc/nixos/configuration should look like this
+# /etc/nixos/configuration should look something like this
+
 { modulesPath, lib, ... }:
 {
   imports = lib.optional (builtins.pathExists ./do-userdata.nix) ./do-userdata.nix ++ [
     (modulesPath + "/virtualisation/digital-ocean-config.nix")
-
-  # add our stuff to 
-  ] ++ [ /home/me/nixos-config/digital-ocean/default.nix ] ;
+  ] 
+        ++ [ /home/me/nixos-config/digital-ocean/keys.nix ] 
+        ++ [ /home/me/nixos-config/digital-ocean/dotfiles.nix ] 
+  ;
 }
+
 
