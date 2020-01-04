13f610f851a38caa8694b060bdd0e1230e74ed66
julian1 <mail@julian1.io>
2020-01-04 15:32:18 +0700

    update

--- nodes/configuration-do.nix
+++ nodes/configuration-do.nix
@@ -6,9 +6,11 @@
     (modulesPath + "/virtualisation/digital-ocean-config.nix")
   ] 
         ++ [ 
-                /home/me/nixos-config/common/basic20.nix   # note v20
-                /home/me/nixos-config/common/keys.nix  
-                /home/me/nixos-config/common/dotfiles.nix 
+            /home/me/nixos-config/common/basic20.nix   # note v20
+            /home/me/nixos-config/common/keys.nix  
+            /home/me/nixos-config/common/dotfiles.nix 
+                
+            /home/me/nixos-config/examples2/myparity/service.nix
         ] 
   ;
 
