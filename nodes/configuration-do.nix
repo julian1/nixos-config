8dc7d48718a1e623968919f5b1f9dd5afe33cedf
julian1 <mail@julian1.io>
2020-01-04 18:05:12 +0700

    update

--- nodes/configuration-do.nix
+++ nodes/configuration-do.nix
@@ -1,19 +1,19 @@
 # /etc/nixos/configuration should look something like this
-# change name of this to dexter... also can we bind some arguments... yes.
+# change name of this node file to dexter...
 
 { modulesPath, lib, ... }:
 {
 
-   
+
   imports = lib.optional (builtins.pathExists ./do-userdata.nix) ./do-userdata.nix ++ [
     (modulesPath + "/virtualisation/digital-ocean-config.nix")
-  ] 
-        ++ [ 
-            /home/me/nixos-config/common/basic20.nix   # note v20
-            /home/me/nixos-config/common/keys.nix  
-            /home/me/nixos-config/common/dotfiles.nix 
-            /home/me/nixos-config/examples2/myparity/service.nix  
-        ] 
+  ]
+    ++ [
+        /home/me/nixos-config/common/basic20.nix   # note v20
+        /home/me/nixos-config/common/keys.nix
+        /home/me/nixos-config/common/dotfiles.nix
+        /home/me/nixos-config/examples2/myparity/service.nix
+    ]
   ;
 
 
