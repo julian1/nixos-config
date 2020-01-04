9b79c0b327d846eff360c4f5dc0a5fec887efaf0
julian1 <mail@julian1.io>
2020-01-04 11:35:34 +0700

    fix build by not having a derivation in a derivation

--- examples2/myparity/default.nix
+++ examples2/myparity/default.nix
@@ -6,32 +6,41 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
 
 let
-  version     = "2.5.12";
+  version     = "2.5.13";
+  sha256      = "0imfdjcg42jfnm897mgjyg0lj4dffsv44z74v0ilwqwqp9g9hwvx";
+  cargoSha256 = "16nf6y0hyffwdhxn1w4ms4zycs5lkzir8sj6c2lgsabig057hb6z";  
+
+  # ok. this now fails. perhaps because we want master branch? or something?
   # plug values in after it complains
-  sha256      = "04b3nxfjqxk7imqizzrd7rcgpqjv5dn84cy1hlbxqn42r4q7f6i1";
-  cargoSha256 = "16nf6y0hyffwdhxn1w4ms4zycs5lkzir8sj6c2lgsabig057hb6z";  # wrong
+  #version     = "2.5.12";
+  #sha256      = "04b3nxfjqxk7imqizzrd7rcgpqjv5dn84cy1hlbxqn42r4q7f6i1";
+  #cargoSha256 = "16nf6y0hyffwdhxn1w4ms4zycs5lkzir8sj6c2lgsabig057hb6z";  # wrong
 
   # version     = "2.5.11";
   #sha256      = "1x2p559g2f30520v3kn46n737l5s1kwrn962dv73s6mb6n1lhs55";
   #cargoSha256 = "16nf6y0hyffwdhxn1w4ms4zycs5lkzir8sj6c2lgsabig057hb6z";
 
-
- # lib fetchFromGitHub rustPlatform cmake openssl pkgconfig systemd
-
-
-  x = import ./parity.nix { inherit version sha256 cargoSha256; }
-    (with nixpkgs; { inherit lib fetchFromGitHub rustPlatform cmake openssl pkgconfig systemd ; })
-    ;
+  #x = import ./parity.nix { inherit version sha256 cargoSha256; }
+  #  (with nixpkgs; { inherit lib fetchFromGitHub rustPlatform cmake openssl pkgconfig systemd ; })
+  #  ;
 
 in
+  import ./parity.nix { inherit version sha256 cargoSha256; }
+    (with nixpkgs; { inherit lib fetchFromGitHub rustPlatform cmake openssl pkgconfig systemd ; }) 
+
+
+  
 
 # x has most of this stuff set. eg.  nativeBuildInputs, buildInputs  etc
 # so why doesn't it build...
 
 # see, https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/rust.section.md
 
+# we already have
+/*
 nixpkgs. stdenv.mkDerivation {
-  name = "rust-env";
+  #name = "rust-env";
+  name = "sssss";
   buildInputs = [
     # Example Run-time Additional Dependencies
     x
@@ -40,6 +49,7 @@ nixpkgs. stdenv.mkDerivation {
   # Set Environment Variables
   RUST_BACKTRACE = 1;
 }
+*/
 
 
 # ok.   it builds a shell but doesn't build???
