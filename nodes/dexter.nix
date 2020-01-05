96f136e211a92757c72674d45a07c6c71ffeb031
julian1 <mail@julian1.io>
2020-01-05 14:14:46 +0700

    tidy

--- nodes/dexter.nix
+++ nodes/dexter.nix
@@ -3,19 +3,15 @@
 
 { modulesPath, lib, ... }:
 {
-
-
   imports = lib.optional (builtins.pathExists ./do-userdata.nix) ./do-userdata.nix ++ [
-    (modulesPath + "/virtualisation/digital-ocean-config.nix")
-  ]
-    ++ [
-        /home/me/nixos-config/common/basic20.nix   # note v20
-        /home/me/nixos-config/common/users.nix
-        /home/me/nixos-config/common/keys-extra.nix
-        /home/me/nixos-config/common/dotfiles.nix
-        /home/me/nixos-config/examples2/myparity/service.nix
-    ]
-  ;
+    (modulesPath + "/virtualisation/digital-ocean-config.nix") ] ++ 
+  [
+    /home/me/nixos-config/common/basic20.nix   # note v20
+    /home/me/nixos-config/common/users.nix
+    /home/me/nixos-config/common/keys-extra.nix
+    /home/me/nixos-config/common/dotfiles.nix
+    /home/me/nixos-config/examples2/myparity/service.nix
+  ];
 
 
   config.networking.hostName = "dexter";
--- nodes/nixos03.nix
+++ nodes/nixos03.nix
@@ -1,17 +1,16 @@
 # actually we don't want to sym link these. because they'll be different for every node
 # unless create specific node.nix instances
+
 { config, pkgs, ... }:
 {
-    imports = [ <nixpkgs/nixos/modules/virtualisation/lxc-container.nix> ]
-        ++ [ 
-
-                /home/me/nixos-config/common/multi-glibc-locale-paths.nix 
-                /home/me/nixos-config/common/basic19.nix   # note. v19
-                /home/me/nixos-config/common/users.nix  
-                /home/me/nixos-config/common/dotfiles.nix 
-                /home/me/nixos-config/examples2/script-service.nix 
-        ]; 
-
+  imports = [ <nixpkgs/nixos/modules/virtualisation/lxc-container.nix> ] ++ 
+  [ 
+    /home/me/nixos-config/common/multi-glibc-locale-paths.nix 
+    /home/me/nixos-config/common/basic19.nix   # note. v19
+    /home/me/nixos-config/common/users.nix  
+    /home/me/nixos-config/common/dotfiles.nix 
+    /home/me/nixos-config/examples2/script-service.nix 
+  ]; 
 
 
   networking.hostName = "nixos03";
