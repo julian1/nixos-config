3084b9ad1b4676ebc058c5c23f5c62cf432925bc
julian1 <mail@julian1.io>
2020-01-05 16:43:06 +0700

    update

---
+++ examples2/welcome.nix
@@ -0,0 +1,16 @@
+# eg. run as nix-shell ./welcome.nix
+
+{ pkgs ? import <nixpkgs> {} }:
+with pkgs;
+
+pkgs.stdenv.mkDerivation {
+  name = "my-example";
+
+  shellHook = ''figlet "Welcome!" | lolcat --freq 0.5'';
+
+  buildInputs = [
+    figlet
+    lolcat
+  ];
+
+}
