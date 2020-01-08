609b4299d32d5f3e2edc1f75e235902b725298d4
julian1 <mail@julian1.io>
2020-01-08 16:54:35 +0700

    tidy

--- examples/bash.nix
+++ examples/bash.nix
@@ -2,17 +2,12 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs;
 
-# this works...
-# eg. cat result/bin/bash
-# shows that it adds the dotfiles. but the path is no good.
-# would need to copy dotfiles into the store somehow
+# OK. this works...
+# it copies ../dotfiles/bashrc into the store 
+# can verify
 
 
-let 
-
-#file = builtins.readFile ( ../dotfiles/bashrc );
-
-myBash = pkgs.symlinkJoin {
+pkgs.symlinkJoin {
   name = "whoot";
   buildInputs = [makeWrapper];
   paths = [ bash ];
@@ -21,11 +16,6 @@ myBash = pkgs.symlinkJoin {
     wrapProgram "$out/bin/bash" --add-flags "--rcfile ${../dotfiles/bashrc}"
 
     '';
+}
 
-    #wrapProgram "$out/bin/bash" --add-flags "--rcfile ${file}"
-    #wrapProgram "$out/bin/bash" --add-flags "--rcfile ./dotfiles/bashrc"
-};
-
-in
-myBash
 
