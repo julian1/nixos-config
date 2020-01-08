9b4f8b0d531c9e017662ff19c516f2f64e516615
julian1 <mail@julian1.io>
2020-01-08 16:38:38 +0700

    tidy

--- examples/bash.nix
+++ examples/bash.nix
@@ -2,8 +2,13 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs;
 
+# this works...
+# eg. cat result/bin/bash
+# shows that it adds the dotfiles. but the path is no good.
+# would need to copy dotfiles into the store somehow
+
 let myBash = pkgs.symlinkJoin {
-  name = "bash";
+  name = "whoot";
   buildInputs = [makeWrapper];
   paths = [ bash ];
   postBuild =
@@ -11,33 +16,7 @@ let myBash = pkgs.symlinkJoin {
     wrapProgram "$out/bin/bash" --add-flags "--rcfile ./dotfiles/bashrc"
     '';
 };
+
 in
-
-
-pkgs.mkShell {
-  name = "swift-env";
-
-  buildInputs = with pkgs; [
-    myBash 
-  ];
-
-  shellHook = ''
-    CC=clang
-  '';
-}
-
-# shellHook can actually choose to replace bash with eg. screen .... 
-
-# what's the difference between mkShell and mkDerivation ???
-
-#stdenv.mkDerivation {
-#  name = "my-example";
-#
-#  buildInputs = [
-#    myBash
-#  ];
-#
-#}
-
-
+myBash
 
