52bddc71d853322c83f1ccd0158412e33c74b12d
julian1 <mail@julian1.io>
2020-02-14 11:11:39 +0700

    nixos03 static ip address

--- nodes/nixos03.nix
+++ nodes/nixos03.nix
@@ -18,6 +18,25 @@
 
   config.networking.firewall.enable = false;
 
+  # static ip address
+  config.networking = {
+    interfaces.host0.ipv4.addresses = [{
+      address = "10.3.0.31";
+      prefixLength = 24;
+    }];
+    defaultGateway = "10.3.0.1";
+    nameservers = [ "8.8.8.8" ];
+  };
+
   # show ip address,
   config.services.mingetty.greetingLine = pkgs.lib.mkForce ''<<< \4, Welcome to NixOS, (\m) - \l >>>'';
 }
+
+
+# usePredictableInterfaceNames = false;
+# usePredictableInterfaceNames = true;
+#  interfaces.eth0.ip4 
+# interfaces.host0.ip4.addresses = [{
+
+# interfaces.host0.ip4 = [{
+
