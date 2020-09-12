fd0db896a2b12a8f322b2ab82df22f19a0e4540b
julian1 <mail@julian1.io>
2020-09-12 16:46:24 +1000

    update

---
+++ examples/maven-and-java.nix
@@ -0,0 +1,22 @@
+/*
+
+  nix-shell  ~/nixos-config/examples/maven-and-java.nix
+  cd freerouting
+  mvn install
+
+*/
+
+with import <nixpkgs> {}; 
+
+callPackage ({ stdenv,   maven, makeWrapper,  jdk,  javaPackages  }: stdenv.mkDerivation {
+ 
+  pname = "maven-and-java";
+  version = "1.0.0";
+
+  nativeBuildInputs = [ maven jdk ];   # needed
+  buildInputs = [ jdk ];                # needed, must be here...
+} ) {} 
+
+
+
+
