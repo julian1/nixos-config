0d4e87eec0f47ce9c12529f54391c986ba319dea
julian1 <mail@julian1.io>
2021-05-12 11:10:13 +1000

    add icestorm

---
+++ examples/icestorm.nix
@@ -0,0 +1,31 @@
+
+{ pkgs ? import <nixpkgs> {} }:
+with pkgs;
+
+# we don't need all the let bindings,
+let
+  x = 1 ;
+  y = 2;
+in
+
+
+# hoogle db written to $HOME/.hoogle. persists through nix-shell restarts
+
+pkgs.stdenv.mkDerivation {
+  name = "my-example";
+
+  # start with ghci!!
+  # shellHook = ''figlet "Welcome!" | lolcat --freq 0.5 && exec ghci'';
+  shellHook = ''figlet "Welcome!" | lolcat --freq 0.5'';
+
+  buildInputs = [
+    figlet
+    lolcat
+
+  yosys 
+  arachne-pnr  
+  icestorm
+
+  ];
+
+}
