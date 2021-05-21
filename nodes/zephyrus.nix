eaca316ad35c50f730c9b77c26e76d4a48fc6021
julian1 <mail@julian1.io>
2021-05-21 19:29:01 +1000

    change timezone

--- nodes/zephyrus.nix
+++ nodes/zephyrus.nix
@@ -56,6 +56,7 @@ in
 
   # Set your time zone.
   # time.timeZone = "Europe/Amsterdam";
+  time.timeZone = "Australia/Hobart";
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
