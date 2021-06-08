8951c0a1ce105b73e5b2d767e9aad2f2e9085b36
julian1 <mail@julian1.io>
2021-06-08 16:13:11 +1000

    add arm dev stuff

---
+++ examples/arm.nix
@@ -0,0 +1,29 @@
+
+{ pkgs ? import <nixpkgs> {} }:
+with pkgs;
+
+pkgs.stdenv.mkDerivation {
+  name = "my-example";
+
+  shellHook = ''figlet "Welcome!" | lolcat --freq 0.5'';
+
+  buildInputs = [
+    figlet
+    lolcat
+
+    gnumake
+
+    # gcc-arm-embedded-9-2019-q4-major    jun 8, 2021
+    gcc-arm-embedded
+    
+    # libopencm3 uses pythong to do something. 
+    python3
+
+    # openocd-0.10.0    jun 8, 2021. 
+    openocd
+    rlwrap
+    # nc comes from somewherei 
+    netcat
+  ];
+
+}
