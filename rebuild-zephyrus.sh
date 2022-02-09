b1cf88636db9a072ea7b0d031d11834b301f5dde
julian1 <mail@julian1.io>
2022-02-10 10:24:11 +1100

    update

--- rebuild-zephyrus.sh
+++ rebuild-zephyrus.sh
@@ -1,4 +1,7 @@
 
-nixos-rebuild build -I nixpkgs=/home/me/devel/nixpkgs/  -I  nixos-config=./nodes/zephyrus.nix     switch
+nixos-rebuild build   -I nixpkgs=/home/me/devel/nixpkgs/  -I  nixos-config=./nodes/zephyrus.nix     switch
+
+# force tarball download after 60seconds instead of default 1hr.
+# nixos-rebuild build --option tarball-ttl 60  -I nixpkgs=/home/me/devel/nixpkgs/  -I  nixos-config=./nodes/zephyrus.nix     switch
 
 
